9986bb3af821015c6c2fd6d37c4c0a4737cc4e8f
Jai Verma <vermajai2004@gmail.com>
2022-05-26 21:13:28 +0530

    added service_provider

--- Pronto_Project_Manager/ProjectManager/views.py
+++ Pronto_Project_Manager/ProjectManager/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
-
+def index1(request):
+    return render(request, 'index1.html')
+    
 def activities(request):
     return render(request, 'activities.html')
 
